a9bb089ea3db4c802388ed647b6e78ef2d2deb2e
刘跃明 <471025316@qq.com>
2019-10-22 17:55:09 +0800

    练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw7/MainActivity.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/MainActivity.java
@@ -54,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(pager);
+
+        pager.setCurrentItem(1);
     }
 
     @Override
--- app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw7/practice/practice03/Practice03OfObjectLayout.java
@@ -52,7 +52,15 @@ public class Practice03OfObjectLayout extends RelativeLayout {
         // 重写 evaluate() 方法，让 PointF 可以作为属性来做动画
         @Override
         public PointF evaluate(float fraction, PointF startValue, PointF endValue) {
-            return startValue;
+            PointF result = new PointF();
+            float dx = endValue.x - startValue.x;
+            float dy = endValue.y - startValue.y;
+
+            float rx = startValue.x + dx * fraction;
+            float ry = startValue.y + dy * fraction;
+            result.set(rx, ry);
+
+            return result;
         }
     }
 }
